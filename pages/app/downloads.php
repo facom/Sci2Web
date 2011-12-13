bee7f392eac2296454db6c0593691a8c953bd4b4
Laptop Root <zuluagajorge@gmail.com>
2011-12-13 08:53:50 -0500

    App pages

---
+++ pages/app/downloads.php
@@ -0,0 +1,78 @@
+<?
+session_start();
+//////////////////////////////////////////////////////////////////////////////////
+//FILE WITH CONTENT
+//////////////////////////////////////////////////////////////////////////////////
+$FILE="$_SESSION[App]-down.html";
+//////////////////////////////////////////////////////////////////////////////////
+?>
+
+<?
+//////////////////////////////////////////////////////////////////////////////////
+//LIBRARY
+//////////////////////////////////////////////////////////////////////////////////
+$RELATIVE="../..";
+include("$RELATIVE/lib/sci2web.php");
+
+//////////////////////////////////////////////////////////////////////////////////
+//VARIABLES
+//////////////////////////////////////////////////////////////////////////////////
+$APPNAME="$_SESSION[App]";
+$APPDIR="$PROJ[APPSDIR]/$APPNAME";
+$APPPATH="$PROJ[APPSPATH]/$APPNAME";
+$FHASH=md5($FILE);
+$LINK="";
+$RID=$PHP["RANDID"];
+$RESULT="";
+
+//////////////////////////////////////////////////////////////////////////////////
+//CHECK PERMISSIONS
+//////////////////////////////////////////////////////////////////////////////////
+if(isset($_SESSION["User"]) and 
+   (strstr($_SESSION["Contributors"],"$_SESSION[User]") or
+    strstr("$PROJ[ROOTEMAIL]","$_SESSION[User]")
+    )
+   ){
+$LINK=<<<LINK
+<div id="editlink_$RID" class="editlink">
+  <a href="JavaScript:void(null)"
+  onclick="toggleToEdition('content_$RID','edition_$RID','editlink_$RID','$COLORS[text]','$PROJ[PROJDIR]/lib/ckfinder')">
+  Edit
+  </a>
+</div>
+LINK;
+   blankFunc();
+   //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+   //SAVE
+   //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+   if(isset($PHP["SaveContent"])){
+     if(!isBlank($PHP["CONTENT_$FHASH"])){
+       $fl=fileOpen("$APPPATH/$FILE","w");
+       fwrite($fl,$PHP["CONTENT_$FHASH"]);
+       fclose($fl);
+     }
+   }
+ }
+
+//////////////////////////////////////////////////////////////////////////////////
+//CONTENT
+//////////////////////////////////////////////////////////////////////////////////
+$CONTENT=shell_exec("cat $APPPATH/$FILE");
+$TabNum=$PHP[TabNum];
+echo<<<CONTENT
+<!--EDIT LINK-->
+<form action="?" method="get" enctype="multipart/form-data">
+$LINK
+<!--NORMAL CONTENT-->
+<div id="content_$RID" style="display:block">
+$CONTENT
+</div>
+<!--EDIT CONTENT-->
+<input id="TabId" type="hidden" name="TabId" value="$TabNum">
+<textarea id="edition_$RID" name="CONTENT_$FHASH" style="display:none;height:100%">
+$CONTENT
+</textarea>
+</form>
+$RESULT
+CONTENT;
+?>
---
+++ pages/app/runs.php
@@ -0,0 +1,279 @@
+<?
+/*
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#          _ ___               _						 #
+#         (_)__ \             | |    						 #
+# ___  ___ _   ) |_      _____| |__  						 #
+#/ __|/ __| | / /\ \ /\ / / _ \ '_ \ 						 #
+#\__ \ (__| |/ /_ \ V  V /  __/ |_) |						 #
+#|___/\___|_|____| \_/\_/ \___|_.__/ 						 #
+#JORGE ZULUAGA (C) 2011  							 #
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# TAB PAGE
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+*/
+//////////////////////////////////////////////////////////////////////////////////
+//LOAD PACKAGE
+//////////////////////////////////////////////////////////////////////////////////
+$RELATIVE="../..";
+include("$RELATIVE/lib/sci2web.php");
+?>
+
+<?
+//////////////////////////////////////////////////////////////////////////////////
+//GLOBAL VARIABLES
+//////////////////////////////////////////////////////////////////////////////////
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//INITIALIZATION
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+$result="";
+$header="";
+$footer="";
+$timeout=2000;
+$conflinknew="$PROJ[BINDIR]/configure.php?Action=New";
+$sname="";
+$sdate="";
+$sstatus="";
+$check="";
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//CHECK AUTHORIZATION
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if(!isset($_SESSION["User"])){
+$result=<<<RUNS
+<center>
+<p class="error">
+You have to signup before to access the application queue
+</p>
+</center>
+RUNS;
+goto end;
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//DIRECTORIES
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+$appname="$_SESSION[AppVersion]";
+$appdir="$PROJ[APPSDIR]/$appname";
+$apppath="$PROJ[APPSPATH]/$appname";
+$runsdir="$PROJ[RUNSDIR]/$_SESSION[User]/$appname";
+$runspath="$PROJ[RUNSPATH]/$_SESSION[User]/$appname";
+$savedbpath="$PROJ[RUNSPATH]/db/$appname";
+
+//////////////////////////////////////////////////////////////////////////////////
+//LOAD ACTION
+//////////////////////////////////////////////////////////////////////////////////
+$ajax_runtable=<<<AJAX
+loadContent
+  (
+   '$PROJ[BINDIR]/ajax-get-runs.php?$PHP[QSTRING]',
+   'runs_table',
+   function(element,rtext){
+     $(element).html(rtext);
+     $('#DIVBLANKET').css('display','none');
+     $('#DIVOVER').css('display','none');
+   },
+   function(element,rtext){
+     $('#DIVBLANKET').css('display','block');
+     $('#DIVOVER').css('display','block');
+   },
+   function(element,rtext){
+   },
+   60000,
+   true
+   );
+$('input[name=RunAll]').attr('checked',false);
+AJAX;
+$onload_runtable=genOnLoad($ajax_runtable,'load');
+
+$ajax_action=<<<AJAX
+submitForm
+  ('formqueue',
+   '$PROJ[BINDIR]/ajax-trans-run.php?',
+   'notaction',
+   function(element,rtext){
+    elid=$(element).attr('id');
+    notDiv(elid,rtext);
+    $ajax_runtable;
+   },
+   function(element,rtext){
+    notDiv(elid,'Processing...');
+    elid=$(element).attr('id');
+   },
+   function(element,rtext){
+     elid=$(element).attr('id');
+   }
+   )
+AJAX;
+
+//////////////////////////////////////////////////////////////////////////////////
+//GENERATE CONTENT
+//////////////////////////////////////////////////////////////////////////////////
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//HEADER
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+$files=listFiles("$runspath/templates","*.conf");
+
+//==================================================
+//START HEADER
+//==================================================
+$header.=<<<HEADER
+<tr>
+<td colspan=10>
+<div style="position:relative">
+<div id="notaction"
+  style="position:absolute;
+	 width:20%;
+	 /*height:100%;
+	 text-height:100%;*/
+	 top:-10px;
+	 left:40%;
+	 background-color:$COLORS[clear];
+	 text-align:center;
+	 border:solid $COLORS[text] 1px;
+	 display:none;
+	 ">
+  Hola
+</div>
+<input id="actionspec" type="hidden" name="Action_Submit" value="None">
+<input type="hidden" name="RunMultiple_Submit" value="true">
+HEADER;
+
+//==================================================
+//CONTROL BUTTONS
+//==================================================
+$actions=$PROJ["Actions"];
+array_unshift($actions,"Remove");
+$links="";
+foreach($actions as $action){
+  $actionlink=$ajax_action;
+$links.=<<<LINKS
+<div class="actionbutton">
+<button class="image" id="Bt_$action" 
+	onclick="$('#actionspec').attr('value','$action');
+		 $ajax_action;
+		 " 
+	onmouseover="explainThis(this)" 
+	explanation="$action"
+	>
+$BUTTONS[$action]
+</button> 
+</div>
+LINKS;
+}
+$header.=<<<BUTTONS
+$links
+BUTTONS;
+
+//==================================================
+//GENERATE LINKS
+//==================================================
+//$actionlink="Open('$conflinknew&Template=Default','Configure','$PROJ[SECWIN]')";
+$actionlink=$ajax_action;
+$header.=<<<HEADER
+<div class="actionbutton">
+HEADER;
+
+//==================================================
+//GENERATE LIST OF TEMPLATES
+//==================================================
+$bugbut=genBugForm("NewFromTemplate","Problems creating new from template");
+$header.=<<<HEADER
+Template: 
+<select name='Template' onchange='popOutHidden(this)'>
+HEADER;
+foreach($files as $file){
+  preg_match("/(.+)\.conf/",$file,$matches);
+  $template=$matches[1];
+  $parts=preg_split("/_/",$template);
+  $tempname=implode(" ",$parts);
+  $header.="<option value='$template'>$tempname";
+}
+$header.=<<<HEADER
+</select>
+<input type="hidden" name="Template_Submit" value="Default">
+HEADER;
+
+$header.=<<<HEADER
+</div>
+<div class="actionbutton">
+<button class="image" id="new" 
+	onclick="$('#actionspec').attr('value','New');
+		 $ajax_action;"
+	onmouseover="explainThis(this)" 
+	explanation="Add run">
+$BUTTONS[Add]
+</button>
+</div>
+HEADER;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//UPDATE
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+$header.=<<<HEADER
+<div class="actionbutton"
+     style="position:absolute;right:0px;top:0px;">
+  <button class="image" onclick="$ajax_runtable">
+    $BUTTONS[Update]
+  </button>
+</div>
+HEADER;
+
+$header.=<<<HEADER
+</div>
+</td></tr>
+HEADER;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//FIELDS
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+$header.=<<<RUNS
+<tr class="header">
+  <td width="2%">
+  <input type="checkbox" name="RunAll" value="off"
+	 onclick="selectAll('formqueue',this)"
+	 onchange="popOutHidden(this)">
+  </td>
+  <td width="20%">
+  Time $sdate</td>
+  <td width="20%">
+  Name of run
+  </td>
+  <td width="20%">
+  Owner
+  </td>
+  <td width="20%">
+  Status$sstatus
+  </td>
+</tr>
+RUNS;
+
+//////////////////////////////////////////////////////////////////////////////////
+//CONTENT
+//////////////////////////////////////////////////////////////////////////////////
+echo<<<RUNS
+<div id="notactions" class="notification" style="display:none"></div>
+<h1>Application Queue</h1>
+$onload_runtable
+<div style="position:relative;padding:5px;border:dashed $COLORS[text] 2px">
+$PROJ[DIVBLANKET]
+$PROJ[DIVOVER]
+<form action="JavaScript:void(null)" id="formqueue">
+<table class="queue">
+<thead>
+$header
+</thead>
+<tbody id="runs_table">
+</tbody>
+<tfooter>
+$footer
+</tfooter>
+</table>
+</form>
+</div>
+RUNS;
+end:
+echo $result;
+?>
